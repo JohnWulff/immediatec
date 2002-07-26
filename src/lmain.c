890378f6f5f16a37ac57a986e009143bec0ca16d
John E. Wulff <immediateC@gmail.com>
2002-07-26 18:47:44 +0000

    RCS	lmain.c	1.2	intermediate version
    test with gram.y and lexc.l to modify C code
    
    CVS-ID: src/lmain.c 1.2

--- src/lmain.c
+++ src/lmain.c
@@ -1,5 +1,5 @@
 static const char lmain_c[] =
-"@(#)$Id: lmain.c,v 1.1 2002/07/08 13:01:29 jw Exp $";
+"@(#)$Id: lmain.c,v 1.2 2002/07/26 18:47:44 jw Exp $";
 /********************************************************************
  *
  *	simple driver for MKS yacc
@@ -8,8 +8,18 @@ static const char lmain_c[] =
 
 #include <stdio.h>
 
+extern void copyAdjust(FILE* fp);
+
+FILE *	yyin;
+
 int
 main(int argc, char **argv)
 {
-	return c_parse();
+	int rv = 0;
+	yyin = fopen(*++argv, "r");
+	rv = c_parse();
+	rewind(yyin);
+	copyAdjust(yyin);
+	
+	return rv;
 } /* main */
