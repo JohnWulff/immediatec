fa6186b1cb7004e759679609aaa9610237e2c812
John E. Wulff <immediateC@gmail.com>
2015-02-24 04:44:37 +0000

    RCS	rpi_rev.c	1.1	intermediate version
    initial revision
    
    CVS-ID: src/rpi_rev.c 1.1

---
+++ src/rpi_rev.c
@@ -0,0 +1,61 @@
+/********************************************************************
+ *
+ *	rpi_rev.c
+ *	adapted from raspberryalphaomega.org.uk by John E. Wulff 2015
+ *
+ *	Returns the following board revisions for differen Raspberry Pi models
+ *		8  (0x08)	Rpi A
+ *		15 (0x0e)	Rpi B	tested by JW
+ *		16 (0x10)	Rpi B+	tested by JW
+ *
+ *	according to http://elinux.org/RPi_HardwareHistory
+ *
+ *    Revision 	Release Date 	Model 	PCB Rev	Memory 	Notes
+ *	Beta 	Q1 2012 	B  	 ? 	256MB 	Beta Board
+ *	0002 	Q1 2012 	B 	1.0 	256MB 	
+ *	0003 	Q3 2012 	B  	1.0 	256MB 	(ECN0001) Fuses mod and D14 removed
+ *	0004 	Q3 2012 	B 	2.0 	256MB 	(Mfg by Sony)
+ *	0005 	Q4 2012 	B 	2.0 	256MB 	(Mfg by Qisda)
+ *	0006 	Q4 2012 	B 	2.0 	256MB 	(Mfg by Egoman)
+ *	0007 	Q1 2013 	A 	2.0 	256MB 	(Mfg by Egoman)
+ *	0008 	Q1 2013 	A 	2.0 	256MB 	(Mfg by Sony)
+ *	0009 	Q1 2013 	A 	2.0 	256MB 	(Mfg by Qisda)
+ *	000d 	Q4 2012 	B 	2.0 	512MB 	(Mfg by Egoman)
+ *	000e 	Q4 2012 	B 	2.0 	512MB 	(Mfg by Sony)
+ *	000f 	Q4 2012 	B 	2.0 	512MB 	(Mfg by Qisda)
+ *	0010 	Q3 2014 	B+ 	1.0 	512MB 	(Mfg by Sony)
+ *	0011 	Q2 2014  Compute Module	1.0 	512MB 	(Mfg by Sony)
+ *	0012 	Q4 2014 	A+ 	1.0 	256MB 	(Mfg by Sony) 
+ *	If you see a "1000" at the front of the Revision, e.g. 10000002
+ *	then it indicates[1] that your Raspberry Pi has been over-volted,
+ *	and your board revision is simply the last 4 digits (i.e. 0002 in this example).
+ *
+ *	Other models need to be checked as they become available
+ *
+ *******************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char	buf[1024];
+char *	needle;
+
+int
+boardrev(void) {
+    FILE* f;
+    int rev;
+
+    if ((f = fopen("/proc/cmdline", "r")) == NULL) {
+	perror("rpi_rev: fopen(/proc/cmdline\")");
+	exit(-1);
+    }
+    fread(buf, 1, 1023, f);
+    fclose(f);
+    if ((needle = strstr(buf, "boardrev")) == NULL ||
+	sscanf(needle, "boardrev=0x%x", &rev) != 1) {
+	fprintf(stderr, "rpi_rev: cannot find \"boardrev\" in:\n%s\n", buf);
+	exit(-1);
+    }
+    return rev & 0xffff;	/* ignore overvolting */
+} /* boardrev */
