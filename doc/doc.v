910b643ac6b9e1fa9e0eef009266096d5eaf21d2
John E. Wulff <immediateC@gmail.com>
2015-09-30 11:34:22 +0000

    RCS	doc.v	1.47	2015/09/30 11:34:22
    update iC.odt iC.pdf
    
    CVS-ID: doc/doc.v 1.47
    CVS-ID: doc/iC.odt 1.47
    CVS-ID: doc/iC.pdf 1.47

--- doc/doc.v
+++ doc/doc.v
@@ -1,5 +1,5 @@
-@(#)$Id: doc.v,v 1.46 2015/06/06 12:17:09 jw Exp $
-iC.pdf		1.46	;
+@(#)$Id: doc.v,v 1.47 2015/09/30 11:34:22 jw Exp $
+iC.pdf		1.47	;
 iC.odp		1.1	;
-iC.odt		1.46	;
+iC.odt		1.47	;
 icc.ods		1.24	;
