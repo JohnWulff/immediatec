cdc72b5551e646dd1754cd11b1410f03ccd381bc
John E. Wulff <immediateC@gmail.com>
2016-01-23 06:00:08 +0000

    RCS	Test0.v	1.1	2016/01/23 06:00:08
    ready for git import
    
    CVS-ID: src/Test0/Test0.v 1.1

---
+++ src/Test0/Test0.v
@@ -0,0 +1,25 @@
+@(#)$Id: Test0.v,v 1.1 2016/01/23 06:00:08 jw Exp $
+a012.is		1.1	;
+a012.sh		1.2	;
+a1.is		1.3	;
+a1.sh		1.3	;
+bar1.sh		1.2	;
+bar2.sh		1.2	;
+bar3.sh		1.2	;
+demo.sh		1.2	;
+p.lt		1.15	;
+pa.lt		1.2	;
+pc.lt		1.11	;
+pd.lt		1.15	;
+pda.lt		1.3	;
+pe.lt		1.11	;
+pg.lt		1.13	;
+pi.lt		1.10	;
+pl.lt		1.10	;
+pm.lt		1.2	;
+pma.lt		1.2	;
+pp.lt		1.7	;
+ps.lt		1.6	;
+sort32i.sh	1.1	;
+sortm28.sh	1.1	;
+zuelke.sh	1.2	;
