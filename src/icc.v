f32a0cfc9372d64ccbf7952e123c787ee6781b89
John Wulff <immediatec@gmail.com>
2017-03-14 20:41:07 +1100

    release version icc_3.1
        Id: src/icc.v 3.1

--- src/icc.v
+++ src/icc.v
@@ -1,86 +1,90 @@
-$Revision icc_2.6 $
-@(#)$Id: icc.v 2.6 $
+$Revision icc_3.1 $
+@(#)$Id: icc.v 3.1 $
 ARTISTIC        1.1     2001/03/02 16:43:55 jw Exp
 COPYING         1.1     2001/03/02 16:43:55 jw Exp
-Makefile.in     1.68    d49bcc2 2016-05-18 12:23:38 +1000 John E Wulff
+Makefile.in     1.72    5055c6f 2017-02-19 17:19:37 +1100 John Wulff
 Msg.pm          1.6     2015/03/06 00:28:04 jw Exp
-README          1.28    3db2e45 2016-04-25 14:42:07 +1000 John E Wulff
-README.RPi      1.3     23ea270 2016-05-15 10:20:16 +1000 John E Wulff
+README          1.30    938d078 2017-03-14 19:00:12 +1100 John Wulff
+README.RPi      1.4     7516017 2017-02-14 20:38:57 +1100 John Wulff
 bar.sav         1.1     2013/02/03 07:14:30 jw Exp
 bar.vcd         1.1     2013/02/03 07:14:30 jw Exp
 barx.vcd        1.1     2013/02/01 23:41:01 jw Exp
 bcm2835.c       1.22    2015/03/29 08:14:44 jw Exp
 bcm2835.h       1.19    2015/03/30 23:01:11 jw Exp
 cexe.h          1.31    2015/10/30 04:40:11 jw Exp
-cexe_t.c        1.1     2013/09/10 08:31:47 jw Exp
-comp.h          1.69    51a0f98 2016-09-06 11:56:28 +1000 John E Wulff
-comp.pl         1.6     2009/08/21 06:01:04 jw Exp
-comp.y          1.117   d49bcc2 2016-05-18 12:23:38 +1000 John E Wulff
-configure       1.8     3db2e45 2016-04-25 14:42:07 +1000 John E Wulff
+cexe_t.c        1.2     7516017 2017-02-14 20:38:57 +1100 John Wulff
+comp.h          1.73    b7cd1b3 2017-03-06 16:26:20 +1100 John Wulff
+comp.tab.c      1.122   938d078 2017-03-14 19:00:12 +1100 John Wulff
+comp.y          1.122   938d078 2017-03-14 19:00:12 +1100 John Wulff
+configure       1.9     7516017 2017-02-14 20:38:57 +1100 John Wulff
 configure.in    1.4     2005/02/01 16:37:44 jw Exp
-genr.c          1.87    51a0f98 2016-09-06 11:56:28 +1000 John E Wulff
-gram.pl         1.7     2014/05/12 13:09:44 jw Exp
-gram.y          1.34    2015/06/05 08:19:18 jw Exp
+cons.tab.c      1.4     b7cd1b3 2017-03-06 16:26:20 +1100 John Wulff
+cons.y          1.4     b7cd1b3 2017-03-06 16:26:20 +1100 John Wulff
+const.c         1.1     5055c6f 2017-02-19 17:19:37 +1100 John Wulff
+genr.c          1.89    7516017 2017-02-14 20:38:57 +1100 John Wulff
+gram.tab.c      1.36    7516017 2017-02-14 20:38:57 +1100 John Wulff
+gram.y          1.36    7516017 2017-02-14 20:38:57 +1100 John Wulff
 iCbox           1.59    23ea270 2016-05-15 10:20:16 +1000 John E Wulff
 iCgpioPUD.c     1.2     2015/04/11 09:30:33 jw Exp
 iClift          1.17    753e41c 2016-07-15 12:15:38 +1000 John E Wulff
-iClive          1.63    4ca7f95 2016-07-11 13:51:08 +1000 John E Wulff
-iCmake          1.47    2015/11/02 11:34:56 jw Exp
-iCman           1.22    8cfe450 2016-06-29 11:52:15 +1000 John E Wulff
+iClive          1.66    9894f95 2017-03-03 11:08:18 +1100 John Wulff
+iCmake          1.50    938d078 2017-03-14 19:00:12 +1100 John Wulff
+iCman           1.24    7516017 2017-02-14 20:38:57 +1100 John Wulff
 iCpiFace.c      1.7     2015/12/26 09:35:24 jw Exp
 iCpiPWM.c       1.3     2015/11/06 07:56:43 jw Exp
 iCrfid          1.1     2014/05/16 03:32:14 jw Exp
-iCserver        1.51    753e41c 2016-07-15 12:15:38 +1000 John E Wulff
+iCserver        1.52    7516017 2017-02-14 20:38:57 +1100 John Wulff
 iCstop          1.13    263b257 2016-04-27 16:28:42 +1000 John E Wulff
 iCtherm         1.5     2016/01/02 22:52:34 jw Exp
 icbegin.c       1.6     2015/11/06 00:47:21 jw Exp
-icc.c           1.76    51a0f98 2016-09-06 11:56:28 +1000 John E Wulff
-icc.h           1.80    2016/01/05 01:09:42 jw Exp
+icc.c           1.78    7516017 2017-02-14 20:38:57 +1100 John Wulff
+icc.h           1.81    044a104 2017-02-18 14:27:32 +1100 John Wulff
 icend.c         1.4     2015/10/31 03:40:08 jw Exp
 icg.pl          1.5     2015/10/16 12:33:47 jw Exp
 icr.c           1.44    2015/10/31 01:26:33 jw Exp
 ict.c           1.70    51a0f98 2016-09-06 11:56:28 +1000 John E Wulff
-immac           1.24    2015/10/19 10:18:25 jw Exp
+immac           1.27    938d078 2017-03-14 19:00:12 +1100 John Wulff
 immax           1.4     2012/12/06 22:28:03 jw Exp
-init.c          1.42    2015/01/22 07:53:37 jw Exp
+init.c          1.45    044a104 2017-02-18 14:27:32 +1100 John Wulff
 init_t.ic       1.4     2014/11/03 11:01:00 jw Exp
 init_t.pl       1.4     2014/11/05 23:31:22 jw Exp
 kbLift.ic       1.12    a2314c3 2016-07-15 17:49:07 +1000 John E Wulff
 kbLift.is       1.5     51a0f98 2016-09-06 11:56:28 +1000 John E Wulff
-lexc.l          1.33    2014/10/22 03:08:15 jw Exp
-lifts.ica       1.5     34ed83e 2016-06-03 17:49:45 +1000 John E Wulff
+lex.yy.c        1.35    7516017 2017-02-14 20:38:57 +1100 John Wulff
+lexc.l          1.35    7516017 2017-02-14 20:38:57 +1100 John Wulff
+lifts.ica       1.6     938d078 2017-03-14 19:00:12 +1100 John Wulff
 link.c          1.33    2015/10/24 03:34:03 jw Exp
-lmain.c         1.18    2009/08/21 06:26:19 jw Exp
-load.c          1.65    51a0f98 2016-09-06 11:56:28 +1000 John E Wulff
-m               1.19    2015/09/29 07:32:32 jw Exp
-makeAll         1.41    3cfef88 2016-04-02 16:46:50 +1100 John E Wulff
-manifest        1.28    263b257 2016-04-27 16:28:42 +1000 John E Wulff
+lmain.c         1.20    5055c6f 2017-02-19 17:19:37 +1100 John Wulff
+load.c          1.66    044a104 2017-02-18 14:27:32 +1100 John Wulff
+m               1.21    5055c6f 2017-02-19 17:19:37 +1100 John Wulff
+makeAll         1.43    5055c6f 2017-02-19 17:19:37 +1100 John Wulff
+manifest        1.29    7516017 2017-02-14 20:38:57 +1100 John Wulff
 mcp23s17.c      1.4     2015/12/23 23:52:15 jw Exp
 mcp23s17.h      1.4     2015/12/18 07:02:24 jw Exp
 misc.c          1.17    2016/01/02 22:54:19 jw Exp
 mklst           1.16    2013/04/19 06:48:33 jw Exp
 ml              1.19    2013/04/19 06:37:45 jw Exp
-outp.c          1.101   51a0f98 2016-09-06 11:56:28 +1000 John E Wulff
+outp.c          1.105   938d078 2017-03-14 19:00:12 +1100 John Wulff
 p_channel.c     1.2     2015/10/16 12:33:47 jw Exp
 pawk.pl         1.8     3db2e45 2016-04-25 14:42:07 +1000 John E Wulff
 perlinstall     1.11    2009/08/21 06:08:40 jw Exp
-pfcad.ic        1.2     2015/11/06 07:04:32 jw Exp
+pfcad.ic        1.3     938d078 2017-03-14 19:00:12 +1100 John Wulff
 pifacecad.c     1.3     2015/03/18 01:30:36 jw Exp
 pifacecad.h     1.4     2015/10/16 12:33:47 jw Exp
 pod2man         1.10    2004/04/03 01:11:21 jw Exp
 pplstfix        1.26    2015/05/17 05:53:25 jw Exp
-rfid.ic         1.2     2015/11/06 07:04:10 jw Exp
+rfid.ic         1.3     938d078 2017-03-14 19:00:12 +1100 John Wulff
 rpi_gpio.c      1.3     2015/04/08 04:33:54 jw Exp
 rpi_gpio.h      1.3     2015/03/05 06:10:16 jw Exp
 rpi_rev.c       1.3     2015/09/29 06:55:10 jw Exp
 rpi_rev.h       1.3     2015/09/29 06:55:10 jw Exp
-rsff.c          1.59    2015/12/04 01:46:49 jw Exp
-scan.c          1.41    2015/12/04 23:57:09 jw Exp
+rsff.c          1.61    5055c6f 2017-02-19 17:19:37 +1100 John Wulff
+scan.c          1.42    e1cef2e 2016-10-15 15:10:36 +1100 John Wulff
 simLift.ic      1.4     4ca7f95 2016-07-11 13:51:08 +1000 John E Wulff
 sortm28_1.sav   1.1     2013/02/27 21:30:43 jw Exp
 sortm28_1.vcd   1.1     2013/02/28 00:36:03 jw Exp
-symb.c          1.22    2014/10/06 07:03:24 jw Exp
+symb.c          1.24    5055c6f 2017-02-19 17:19:37 +1100 John Wulff
 tcpc.c          1.27    2015/11/06 22:48:02 jw Exp
 tcpc.h          1.21    2015/10/18 23:29:08 jw Exp
-therm.ic        1.2     2015/11/06 10:51:30 jw Exp
+therm.ic        1.3     938d078 2017-03-14 19:00:12 +1100 John Wulff
 vcd2sav         1.1     2013/03/03 11:54:38 jw Exp
